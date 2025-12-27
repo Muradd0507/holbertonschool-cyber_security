28d669033287a881eaa6f10f3b5842104857a512
Muradd0507 <muradqulizade75@gmail.com>
2025-12-27 09:56:00 -0500

    aaa

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-awk --csv whois Registrant Admin Tech
+whois "$1" | awk '
+/^(Registrant|Admin|Tech)/{s=$1}
+/:/{
+  f=$1; sub(/^[^:]+: */,"")
+  if(f=="Street") print s" Street,"$0" "
+  else if(f=="Phone"||f=="Fax") print s" "f","$0
+  else if(f=="Ext") print s" "p" Ext:,"
+  else print s" "f","$0
+  p=f
+}
+' > "$1.csv"
