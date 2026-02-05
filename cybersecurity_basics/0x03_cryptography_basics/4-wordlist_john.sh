9138b3afec2d893ea5b1db5db42ceefa048eb483
Muradd0507 <muradqulizade75@gmail.com>
2026-02-05 10:52:29 -0500

    aaa

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-john --format=raw-md5 --wordlist=rockyou.txt "$1"
+john --format=raw-md5 --wordlist=/usr/share/wordlists/rockyou.txt "$1"
 john --show --format=raw-md5 "$1" | cut -d: -f2 > 4-password.txt
